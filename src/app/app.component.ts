dad6945abd534403c782ff69a7fe9005df1c1187
Junior Rojas <rojasjuniore@gmail.com>
2017-12-08 23:11:51 -0500

    :)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,92 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+declare const Twitch: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
+
+  CLIENT_ID = 'yuehfgwoztltlj94a8p6a3jlvb8fmb';
   title = 'app';
+
+  user: Object;
+  stream_key = '';
+  hidden: boolean;
+
+
+  constructor() {
+
+    Twitch.init(
+      {
+        clientId: this.CLIENT_ID,
+        redirect_uri: 'http://localhost:4200/#/'
+      }, (error, status) => {
+        if (status.authenticated) {
+          this.getUser();
+          this.channel();
+          this.hidden = true;
+        } else {
+          this.hidden = false;
+          console.log(error);
+        }
+      });
+  }
+
+  public login() {
+    Twitch.login({
+      redirect_uri: 'http://localhost:4200/#/',
+      scope: ['user_read', 'channel_read', 'channel_editor'],
+
+    }, );
+  }
+
+  public logout() {
+    Twitch.logout((error) => {
+      // the user is now logged out
+      console.log(error);
+    });
+
+    this.user = '';
+    localStorage.clear();
+    sessionStorage.removeItem('twitch_oauth_session');
+    setTimeout(() => {
+      location.reload();
+    }, 1000);
+
+  }
+
+  public getUser() {
+    Twitch.api(
+      {
+        method: 'user'
+      }, (error, user) => {
+
+        this.user = {
+          bio: user.bio,
+          created_at: user.created_at,
+          display_name: user.display_name,
+          email: user.email,
+          logo: user.logo,
+          name: user.name
+        };
+        console.log(this.user);
+
+      });
+  }
+
+  public channel() {
+    Twitch.api(
+      {
+        method: 'channel'
+      }, (error, channel) => {
+        this.stream_key = channel.stream_key;
+      });
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { MaterialModule } from './material.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-
+import {
+  MatButtonModule,
+  MatMenuModule,
+  MatToolbarModule,
+  MatIconModule,
+  MatCardModule
+} from '@angular/material';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MaterialModule,
+    BrowserAnimationsModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
